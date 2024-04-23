fb79cbea9330daff9185f780ce5b628542122410
pmsese <pauladriansese@gmail.com>
2024-04-23 15:06:14 -0700

    Finished Spawning Prototype

--- TD385/Assets/Scripts/Enemy.cs
+++ TD385/Assets/Scripts/Enemy.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private float speed;
+    private int despawnX;
 
     // Start is called before the first frame update
     void Start()
     {
         speed = 5;
+        despawnX = -12;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > -12 )
+        if (transform.position.x > despawnX)
         {
             Vector3 pos = transform.position;
             pos.x -= speed * Time.deltaTime;
             transform.position = pos;
         }
+        else
+        {
+            Destroy(transform.gameObject);
+        }
+    }
 
-
+    public void setSpeed(float speed)
+    {
+        this.speed = speed;
     }
 }
