3d02a66745d94c553eeb5668810e87014e6ce140
Zath <zathaxx@gmail.com>
2024-05-21 20:46:39 -0700

    Replace sword and cannon ball and add text above gold pile

--- TD385/Assets/Scripts/GoldPileBehaviour.cs
+++ TD385/Assets/Scripts/GoldPileBehaviour.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GoldPileBehaviour : MonoBehaviour
 {
     public Sprite goldSprite;
     private Bounds pileBounds;
+    private TextMeshProUGUI goldText;
     public int goldAmount; 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +22,15 @@ public class GoldPileBehaviour : MonoBehaviour
             e.transform.position = new Vector3(randomX, randomY, 0f);
             e.name = "Gold Bar " + i;
         }
+        goldText = GameObject.Find("GoldText").transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        goldText.outlineWidth = 0.2f;
+        goldText.outlineColor = new Color32(0, 0, 0, 255);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        goldText.text = "" + transform.childCount;
     }
 
     void OnTriggerEnter2D(Collider2D other) {
